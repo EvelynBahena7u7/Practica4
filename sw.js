7738577739984c6989049c0edf86d8375de8f5c1
EvelynBahena <20193tn107@utex.edu.mx>
2022-09-27 22:14:31 -0500

    cambios 27/09/22

---
+++ sw.js
@@ -0,0 +1,38 @@
+self.addEventListener('install',(event)=>{
+    console.log('sw: Instalado');
+    
+    const promiseCache= caches.open('cache-v1').then((cache)=>{
+        return cache.addAll(
+             [
+                 '/',
+                 '/index.html',
+                '/pages/suma.html',
+                '/pages/resta.html',
+                '/pages/multi.html',
+                '/pages/divi.html',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
+
+             ]
+         );
+     })
+   /*caches.keys().then((keys)=>{
+    console.log(keys);
+   })*/
+    
+})
+
+self.addEventListener('fetch', (event)=>{
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            if(response){
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+    
+
+
+
