49c37a09cf93d6f0c9ca359c2b4ab89e5dbb7cfa
EvelynBahena7u7 <100085522+EvelynBahena7u7@users.noreply.github.com>
2022-09-27 22:55:30 -0500

    Add files via upload
    prueba

--- sw.js
+++ sw.js
@@ -2,20 +2,15 @@ self.addEventListener('install',(event)=>{
     console.log('sw: Instalado');
     
     const promiseCache= caches.open('cache-v1').then((cache)=>{
-        return cache.addAll(
-             [
-                 '/',
-                 './index.html',
-                './pages/suma.html',
-                './pages/resta.html',
-                './pages/multi.html',
-                './pages/divi.html',
-                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
-                'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css'
-
-             ]
-         );
-     })
+        cache.add('/');
+        cache.add('/index.html');
+        cache.add('/pages/suma.html');
+        cache.add('/pages/resta.html');
+        cache.add('/pages/multi.html');
+        cache.add('/pages/divi.html');
+        cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
+        cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+    })
      event.waitUntil(promiseCache);
     
 })
