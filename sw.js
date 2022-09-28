c884b16f6cdc57fbad6d24bae2e7e5b36176b13c
EvelynBahena7u7 <100085522+EvelynBahena7u7@users.noreply.github.com>
2022-09-27 23:04:04 -0500

    Add files via upload
    j

--- sw.js
+++ sw.js
@@ -2,12 +2,11 @@ self.addEventListener('install',(event)=>{
     console.log('sw: Instalado');
     
     const promiseCache= caches.open('cache-v1').then((cache)=>{
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/pages/suma.html');
-        cache.add('/pages/resta.html');
-        cache.add('/pages/multi.html');
-        cache.add('/pages/divi.html');
+    
+        cache.add('https://evelynbahena7u7.github.io/Practica4/pages/suma.html');
+        cache.add('https://evelynbahena7u7.github.io/Practica4/pages/resta.html');
+        cache.add('https://evelynbahena7u7.github.io/Practica4/pages/multi.html');
+        cache.add('https://evelynbahena7u7.github.io/Practica4/pages/divi.html');
         cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
         cache.add('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
     })
